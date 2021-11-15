f30a7e51c4f6eaa0fb9e8853b2c3e1576608b7c3
ShadowGamb <45771418+ShadowGamb@users.noreply.github.com>
2021-11-15 21:04:50 +0100

    Drittes Praktikum

--- Praktikum_3/P3_A4.py
+++ Praktikum_3/P3_A4.py
@@ -5,9 +5,11 @@ L3 = ['P', 'y', 't', 'h', 'o', 'n']
 
 def cleaner(zu_reinigen):
 
+    # Variablen für die neue gereinigte Liste und die Länge zum Durchlaufen der Liste
     gereinigte_liste = []
     laenge = len(zu_reinigen)
 
+    # For-Schleife zum Aussortieren der Strings und um an die neue Liste anzuhängen
     for i in range(0, laenge):
         if isinstance(zu_reinigen[i], str):
             gereinigte_liste.append(zu_reinigen[i].lower())
@@ -16,10 +18,12 @@ def cleaner(zu_reinigen):
 
 def palindrome(liste):
 
+    # Aufruf der Cleaner-Funktion
     gereinigt = cleaner(liste)
 
+    # umgedreht für das umkehren der liste fürs Palindrom
     umgedreht = list(reversed(gereinigt))
-    wort = ''.join(gereinigt)
+    wort = '-'.join(gereinigt)
 
     return (wort.capitalize(), umgedreht==gereinigt)
 
