e6bea82965adb396912b6fdfa0895c7e86aefb41
ShadowGamb <45771418+ShadowGamb@users.noreply.github.com>
2021-11-29 18:16:50 +0100

    Viertes Praktikum

--- Praktikum_4/P4_A1.py
+++ Praktikum_4/P4_A1.py
@@ -1,16 +1,23 @@
 import string
 def morse_code(eingabe):
-    eingabe = eingabe.lower()
-    eingabe = [zeichen for zeichen in eingabe if zeichen.isalnum()] #isalpha() sind nur buchstaben
+
+    #Eingabe vergrößer für Vereinheitlichung
+    eingabe = eingabe.upper()
     eingabe = " ".join(eingabe)
-    print(eingabe)
-    morse_code = ".-,-...,-.-.,-..,.,..-.,--.,....,..,.---,-.-,.-..,--,-.,---,.--.,--.-,.-.,...,-,..-,...-,.--,-..-,-.--,--..,-----,.----,..---,...--,....-,.....,-....,--...,---..,----."
-    morse_code = morse_code.split(",")
-    let_dig= list(string.ascii_lowercase) + list(string.digits)
-    dictionary = {k:v for k, v in zip(let_dig, morse_code)}
-    vergleich = [dictionary[k] for k in eingabe if k in dictionary]
-    vergleich = " ".join(vergleich)
-    return vergleich
+
+    # Sonderzeichen rausfiltern
+    eingabe = [zeichen for zeichen in eingabe if zeichen.isalnum()]
+
+    morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '-----', '.----', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.']
+
+    #mit der Stringbibliothek lassen sich alle benötigten Sachen holen und zusammenführen
+    buchst_und_zahlen = list(string.ascii_uppercase) + list(string.digits)
+    morse_zeichen_uebersetzung = {k:v for k, v in zip(buchst_und_zahlen, morse)}
+
+    #Wort/Satz übersetzung und zurückgeben
+    ergebnis = [morse_zeichen_uebersetzung[k] for k in eingabe if k in morse_zeichen_uebersetzung]
+    ergebnis = " ".join(ergebnis)
+    return ergebnis
 
 assert morse_code("Hello, World!") == ".... . .-.. .-.. --- .-- --- .-. .-.. -.."
 assert morse_code("Hello,World!") == morse_code("hello world")
--- Praktikum_4/P4_A2.py
+++ Praktikum_4/P4_A2.py
@@ -1,19 +1,17 @@
-def filter_alphanumeric(string):
-    import re
+import re
 
-    longestWord = ""
-    string = re.sub('\W+', ' ', string)  # es wird nach allen nicht Alphanumerischen Zeichen(Sondernzeichen) im String gesucht und entfernt
+def filter_alphanumeric(eingabe):
+    eingabe = re.sub('\W+', ' ', eingabe)  # es wird nach allen nicht Alphanumerischen Zeichen(Sondernzeichen) im String gesucht und entfernt
+    return eingabe
 
+def longest_word(eingabe2):
+    gefiltert = filter_alphanumeric(eingabe2)
+    word_list = gefiltert.split()
 
-    word_list = string.split()
     max_len = len(max(word_list, key=len))
-    max_words = [word for word in word_list if len(word)==max_len]
-    print(max_words)
+    max_words = {word for word in word_list if len(word)==max_len}
 
-test3 = "HEute heute ist , das Wetter schön schoen !!!!!!"
-
-filter_alphanumeric(test3)
-# longest_word(test3) #== (6, {'Wetter', 'schoen'})
+    return (max_len, max_words)
 
 test1 = """
 Python ist eine universelle, \t \t üblicherweise interpretierte, 
@@ -23,6 +21,7 @@ So werden beispielsweise Blöcke nicht durch geschweifte Klammern,
 sondern durch Einrückungen strukturiert. \n
 Quelle: Wikipedia, Programmiersprache, Python
 """
+
 test2 = """
 Implementieren Sie die Funktion longest_word die in einem Inputstring das längste Wort 
 (es können mehrere Wörter die gleiche Länge haben) findet und diese maximale Länge als auch alle 
@@ -30,6 +29,9 @@ Wörter, die diese Länge haben, in einer Liste als Rückgabewert ausgibt.
 Sie dürfen jeder Gruppe von Zeichen, die kein Leerzeichen sind, als Wort interpretieren, 
 sogar wenn diese Zahlen oder Satzzeichen sind.
 """
-filter_alphanumeric(test3)
 
-# help(max)
+test3 = "HEute heute ist , das Wetter schön schoen !!!!!!"
+
+assert longest_word(test1) == (18, {'Programmiersprache'})
+assert longest_word(test2) == (14, {'Implementieren', 'interpretieren'})
+assert longest_word(test3) == (6, {'Wetter', 'schoen'})
